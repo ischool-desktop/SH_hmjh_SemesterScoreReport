ba528ab5943905157dfe1f7bae6d12c22399d8b6
chentaichen <ct.chen@ischool.com.tw>
2014-03-21 14:41:55 +0800

    和美_期末學期成績單_專案檔(epost)

---
+++ SH_hmjh_SemesterScoreReport/Permissions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SH_hmjh_SemesterScoreReport
+{
+    class Permissions
+    {
+        public static string KeyHmjhSemesterScoreReport { get { return "SHSchool.SH_hmjh_SemesterScoreReport.cs"; } }
+        public static bool IsEnableHmjhSemesterScoreReport
+        {
+            get
+            {
+                return FISCA.Permission.UserAcl.Current[KeyHmjhSemesterScoreReport].Executable;
+            }
+        }
+    }
+}
